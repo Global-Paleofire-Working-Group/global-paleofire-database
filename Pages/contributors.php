f91c6e783fd75ec2553b308d3444740754c586a1
ykato <yuji.kato@univ-fcomte.fr>
2019-12-18 15:40:32 +0100

    Creation of a contributors page

---
+++ Pages/contributors.php
@@ -0,0 +1,24 @@
+<!--
+ fichier Pages/terms_of_use.php
+ Auteur : XLI janvier 2016 
+ nouvelle version de la charte 
+-->
+<div class="home">
+    <div class="header">
+        <h3>Contributors of the Global PaleoFire Database</h3>
+    </div>
+    <div>
+        <p></p>
+    </div>
+</div>
+<script type="text/javascript">
+    var add = "ma";
+    add += "ilto:paleofire";
+    add += "@";
+    add += "gmail.com?";
+    add += "Subject=Contact%20from%20term%20of%20use%20page";
+    $("#mes").mouseover(function(){
+        $(this).attr("href", add);
+    });
+    
+</script>
